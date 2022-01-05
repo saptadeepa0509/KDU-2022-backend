b5bdb1647f7c884ce5841c1a46cbe535dcc5d294
saptadeepa <saptadeepa.ghosh@kickdrumtech.com>
2022-01-05 12:30:11 +0530

    homework1

--- q1.java
+++ q1.java
@@ -6,12 +6,21 @@ public class Main
         System.out.println("Enter a number");
         int n = sc.nextInt();
 
-        for(int i = 1;i<=n;i++)
+        int a = 1;
+        int b = 4;
+        int c = 7;
+        int sum = 0;
+        System.out.print(a+" "+b+" "+c+" ");
+        while(sum<n)
         {
-            if(i%4!=0)
-            {
-                System.out.print(i*i+" ");
-            }
+
+            sum = a+b+c;
+            if(sum>n)break;
+            System.out.print(sum+" ");
+            a = b;
+            b = c;
+            c = sum;
+
         }
     }
 }
