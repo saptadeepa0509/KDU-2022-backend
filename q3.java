4e186bf1952b0530617b7c9b7d2b14173f077b4f
saptadeepa <saptadeepa.ghosh@kickdrumtech.com>
2022-01-05 12:38:54 +0530

    adding changes of 3,4,5 question

--- q3.java
+++ q3.java
@@ -3,22 +3,23 @@ public class Main
 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str=sc.nextLine();
-        int i = 0, j = str.length() - 1;
-        int flag =0;
-        while (i < j) {
-            if (str.charAt(i) != str.charAt(j))
-            {
-                flag=1;
-                break;
-            }
-            i++;
-            j--;
-        }
-        if(flag==1) System.out.print("false");
-        else
-            System.out.print("true");
+        System.out.println("Enter a number");
+        int n = sc.nextInt();
 
+        int a = 1;
+        int sum = 0;
+        int x = 0;
+        int i = 1;
+        System.out.print(a+" ");
+        while(i<n)
+        {
+            x++;
+            if(x%3==0)continue;
+
+            i = i+x*4;
+            if(i>n)break;
+            System.out.print(i+" ");
+
+        }
     }
 }
-
--- q5.java
+++ q5.java
@@ -3,24 +3,21 @@ public class Main
 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number");
-        int n = sc.nextInt();
-        int sum=0;
-        for(int i=1;i<n;i++)
-        {
-            int a=i;
-            while(a>0)
+        String str=sc.nextLine();
+        int i = 0, j = str.length() - 1;
+        int flag =0;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j))
             {
-                int digit = a % 10;
-                sum = sum + digit;
-                a = a / 10;
+                flag=1;
+                break;
             }
-            if(sum%3==0){
-                System.out.print(i +" ");
-
-            }
-            sum=0;
+            i++;
+            j--;
         }
+        if(flag==1) System.out.print("false");
+        else
+            System.out.print("true");
 
     }
 }
